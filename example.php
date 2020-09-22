293d5e43270b4506050d9ccbc754e2fad6ded52f
Jonathan van 't Ende <jvantende@onetoweb.nl>
2020-09-22 10:48:35 +0200

    initial commit

---
+++ example.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Onetoweb\Tuindeco\Client;
+
+$apiKey = 'api_key';
+$secret = 'secret';
+
+$client = new Client($apiKey, $secret);
+
+// get products
+$products = $client->getProducts();
+
+// create order
+$order = $client->createOrder([
+    'customer_firstname' => 'Hessel',
+    'customer_lastname' => 'Kramer',
+    'customer_email' => 'test@test.nl',
+    'customer_phone' => '0123123123123123',
+    'customer_company' => 'Asscher Holding B.V.',
+    'customer_street' => 'Woerstraat',
+    'customer_housenumber' => '6',
+    'customer_housenumber_extension' => 'A',
+    'customer_postalcode' => '7751CE',
+    'customer_city' => 'Dalen',
+    'customer_country_iso' => 'NL@1',
+    'customer_ref' => 'INKOOP_Duitsland12',
+    'shipment_code' => 'DDP',
+    'article_number[]' => '11.213123',
+    'article_quantity[]' => '11',
+    'article_number[]' => '40.7065OG',
+    'article_quantity[]' => '13',
+    'article_number[]' => '1.12400P',
+    'article_quantity[]' => '100',
+    'article_number[]' => '32424',
+    'article_quantity[]' => '11',
+    'delivery_weeknr' => '50'
+]);
---
+++ src/Client.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Onetoweb\Tuindeco;
+
+/**
+ * Tuindeco Api Client.
+ * 
+ * @author Jonathan van 't Ende <jvantende@onetoweb.nl>
+ * @copyright Onetoweb B.V.
+ */
+class Client
+{
+    /**
+     * @var string
+     */
+    private $apiKey;
+    
+    /**
+     * @var string
+     */
+    private $secret;
+    
+    /**
+     * @param string $apiKey
+     * @param string $secret
+     */
+    public function __construct(string $apiKey, string $secret)
+    {
+        $this->apiKey = $apiKey;
+        $this->secret = $secret;
+    }
+    
+    /**
+     * Get products.
+     * 
+     * @return \stdClass
+     */
+    public function getProducts(): \stdClass
+    {
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://feed.tuindeco.shop/json/full/",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => ['apikey' => $this->apiKey,'secret' => $this->secret],
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        
+        return json_decode($response);
+    }
+    
+    /**
+     * Create Order.
+     * 
+     * @param array $order
+     * 
+     * @return \stdClass
+     */
+    public function createOrder(array $order): \stdClass
+    {
+        $order['apikey'] = $this->apiKey;
+        $order['secret'] = $this->secret;
+        
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.tuindeco.shop/v1/order/create/",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $order,
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        
+        return json_decode($response);
+    }
+}
